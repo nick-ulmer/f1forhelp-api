956d39c84d349b9b91a9cae333d32b109343702b
Flamerate1 <nball1000@gmail.com>
2026-05-25 18:30:36 -0400

    Added swagger and finished MySQL table creation. Basic backend now functions.

--- app/Http/Controllers/CounterController.php
+++ app/Http/Controllers/CounterController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Counter;
+use OpenApi\Attributes as OA;
 
+#[OA\Info(title: "F1ForHelp API", version: "1.0.0")]
 class CounterController extends Controller
 {
+    #[OA\Get(path: "/api/counter", summary: "Get current count")]
+    #[OA\Response(response: 200, description: "Success")]
     public function getCount()
     {
         $counter = Counter::firstOrCreate(['id' => 1], ['count' => 0]);
         return response()->json(['count' => $counter->count]);
     }
 
+    #[OA\Post(path: "/api/counter/increment", summary: "Increment the counter")]
+    #[OA\Response(response: 200, description: "Success")]
     public function increment()
     {
         $counter = Counter::firstOrCreate(['id' => 1], ['count' => 0]);
